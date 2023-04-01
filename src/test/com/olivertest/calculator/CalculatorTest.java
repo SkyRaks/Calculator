81ba9cbc3fbda56098f43e0a48bbe9fd2bbb7e44
oleh2 <nano.volosin2016@gmail.com>
2023-04-01 14:57:17 -0400

    Initial commit

---
+++ src/test/com/olivertest/calculator/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.olivertest.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void canAddZeroPlusZero() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum, "Was expecting sum of 0");
+    }
+}
